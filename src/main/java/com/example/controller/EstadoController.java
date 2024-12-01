e9e12f91ec15338bca9b09f92a4e9b510aa04cde
pjonnathan <jonnathanmatos846@gmail.com>
2024-12-01 13:53:08 -0300

    Create data base

---
+++ src/main/java/com/example/controller/EstadoController.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import com.example.models.entity.Estado;
+import com.example.repositorys.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class EstadoController {
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    @GetMapping("/cadastroEstado")
+    private ModelAndView cadastra(Estado estado){
+        ModelAndView mv = new ModelAndView("administrativo/estado/cadastro");
+        mv.addObject("estado", estado);
+        return mv;
+    }
+}
---
+++ src/main/java/com/example/models/entity/Estado.java
@@ -0,0 +1,49 @@
+package com.example.models.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_estado")
+public class Estado implements Serializable {
+    private static final long  serialVersionUID = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String sigle;
+
+    public Estado(){}
+
+    public Estado(Long id, String name, String sigle) {
+        this.id = id;
+        this.name = name;
+        this.sigle = sigle;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSigle() {
+        return sigle;
+    }
+
+    public void setSigle(String sigle) {
+        this.sigle = sigle;
+    }
+}
